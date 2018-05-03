c663196b6098d8da6b874c8f1a3997f6e3c06039
Thức Nguyễn Quy <thucnguyenquy@gmail.com>
2018-05-03 12:33:14 +0700

    aaa

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,10 +18,30 @@ $(document).ready(function () {
 function scrollFunction() {
     var tmp = $("#contact").offset().top;
     if (document.body.scrollTop > tmp  || document.documentElement.scrollTop > tmp ) {
-        $("#myBtn").show();console.log(tmp);
+        $("#myBtn").show();
     } else {
-        $("#myBtn").hide();console.log("no",tmp);
+        $("#myBtn").hide();
     }
+}
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+function subscribe() {
+    let email = $("#email").val();
+    if(email){
+        if(validateEmail(email)){
+            alert(email);
+        }
+        else{
+            alert("Please enter your email again!");
+        }
+    }
+    else{
+        alert("You should enter your email before!");
+    }
+    
 }
 function createSlick() {
 
@@ -50,6 +70,10 @@ function createSlick() {
             }
         ]
     });
+    // $(".slider").not('.slick-initialized').slick({
+    //     prevArrow:"<img class='a-left control-c prev slick-prev' src='../img/pre_arr.png'>",
+    //     nextArrow:"<img class='a-right control-c next slick-next' style='transform: scaleX(-1);-webkit-transform: scaleX(-1)' src='../img/pre_arr.png'>"
+    // });
 
 }
 
